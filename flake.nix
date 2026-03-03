f84d3228473b1235828ac9b55ae3e2a14daf9fb9
Merlin Attila Fejzuli <merlin@fejzuli.com>
2026-03-03 03:04:30 +0100

    batman

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Personal flake templates";
+  inputs = {
+    flake-parts = {
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+      url = "github:hercules-ci/flake-parts";
+    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    treefmt-nix = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:numtide/treefmt-nix";
+    };
+  };
+  outputs =
+    inputs@{ flake-parts, treefmt-nix, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      flake.templates = {
+        rust = {
+          path = ./rust;
+          description = "Rust flake with fenix";
+          welcomeText = ''
+            # Getting started
+            - Change toolchain in flake.nix by setting the fenix variable.
+              Check https://github.com/nix-community/fenix?tab=readme-ov-file#usage
+              for possible values.
+            - Run `direnv allow`
+            - Run `cargo init`
+          '';
+        };
+      };
+
+      imports = [ treefmt-nix.flakeModule ];
+
+      perSystem.treefmt.programs = {
+        just.enable = true;
+        nixfmt.enable = true;
+      };
+
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    };
+}
---
+++ rust/flake.nix
@@ -0,0 +1,85 @@
+{
+  inputs = {
+    fenix = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/fenix";
+    };
+    flake-parts = {
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+      url = "github:hercules-ci/flake-parts";
+    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    treefmt-nix = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:numtide/treefmt-nix";
+    };
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          inputs',
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        let
+          fenix = inputs'.fenix.packages.stable;
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages =
+              let
+                formatters = lib.attrValues config.treefmt.build.programs;
+                rust = fenix.withComponents [
+                  "cargo"
+                  "clippy"
+                  "rust-analyzer"
+                  "rust-src"
+                  "rustc"
+                  "rustfmt"
+                ];
+                treefmt = config.treefmt.build.wrapper;
+              in
+              [
+                pkgs.just
+                rust
+                treefmt
+              ]
+              ++ formatters;
+          };
+
+          treefmt.programs = {
+            just.enable = true;
+            nixfmt.enable = true;
+            rustfmt = {
+              enable = true;
+              package = fenix.rustfmt;
+            };
+          };
+        };
+
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    };
+}
